362e5cf5f802592764720ab2c811ef3efe83e591
sagisaka2 <sagisaka2>
2017-07-28 10:18:02 +0900

    DBの設定を行った

--- src/main/java/com/spring/app/controller/ProductsRestController.java
+++ src/main/java/com/spring/app/controller/ProductsRestController.java
@@ -24,76 +24,91 @@ import org.springframework.web.multipart.MultipartFile;
 import com.spring.app.ProductsService;
 import com.spring.app.model.Product;
 
-@RestController  
-@RequestMapping("api/product")  
+@RestController
+@RequestMapping("api/product")
 public class ProductsRestController {
-	@Autowired  
-	ProductsService service;  
+	@Autowired
+	ProductsService service;
 
 	// 顧客全件取得
-	@RequestMapping(method=RequestMethod.GET,value="/all")  
+	@RequestMapping(method=RequestMethod.GET,value="/all")
 	public List<Product> getproduct() {
 		return service.findAll();
 	}
 
 	// 顧客一件取得
-	@RequestMapping(method=RequestMethod.GET, value="{id}")  
+	@RequestMapping(method=RequestMethod.GET, value="{id}")
 	public Product getproduct(@PathVariable Integer id) {
 		return service.findOne(id);
 	}
 
 	//　顧客取得
-	@RequestMapping(method=RequestMethod.POST, value="/sam")  
+	@RequestMapping(method=RequestMethod.POST, value="/sam")
 	public List<Product> getValueproduct(@RequestBody Product product) {
 		return service.find(product.getName());
 	}
 
-//	// 顧客一件作成
-//	@RequestMapping(method=RequestMethod.POST)  
-//	@ResponseStatus(HttpStatus.CREATED)  
-//	public product postproduct(@RequestBody product product) {
-//		return service.create(product);
-//	}
+	//	// 顧客一件作成
+	//	@RequestMapping(method=RequestMethod.POST)
+	//	@ResponseStatus(HttpStatus.CREATED)
+	//	public product postproduct(@RequestBody product product) {
+	//		return service.create(product);
+	//	}
 
 	// 顧客一件更新
-	@RequestMapping(method=RequestMethod.POST, value="{id}")  
+	@RequestMapping(method=RequestMethod.POST, value="{id}")
 	public Product putproduct(@PathVariable Integer id,
-			HttpServletResponse response, @RequestParam String name, @RequestParam String introduction, @RequestParam String price,@RequestParam MultipartFile file) {
+	HttpServletResponse response, @RequestParam String name, @RequestParam String introduction, @RequestParam String price,@RequestParam MultipartFile file) {
+
+		if (file.isEmpty()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return null;
+		}
 		Product product = new Product();
 		product.setName(name);
 		product.setIntroduction(introduction);
 		product.setPrice(price);
 		product.setImageUrl(file.getOriginalFilename());
 		product.setId(id);
+		try {
+			BufferedInputStream in =
+			new BufferedInputStream(file.getInputStream());
+			BufferedOutputStream out =
+			new BufferedOutputStream(new FileOutputStream("src/main/resources/static/image/" + file.getOriginalFilename()));
+			FileCopyUtils.copy(in, out);
+		} catch (IOException e) {
+			throw new RuntimeException("Error uploading file.", e);
+		}
 		return service.update(product);
 	}
 
 	// 顧客一件削除
-	@RequestMapping(method=RequestMethod.DELETE, value="{id}")  
-	@ResponseStatus(HttpStatus.NO_CONTENT)  
+	@RequestMapping(method=RequestMethod.DELETE, value="{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteproduct(@PathVariable Integer id) {
 		service.delete(id);
 	}
 
+	// 顧客一件登録
 	@RequestMapping(method=RequestMethod.POST)
 	public Product handle(HttpServletResponse response, @RequestParam String name, @RequestParam String introduction, @RequestParam String price,@RequestParam MultipartFile file){
-		
+
+		if (file.isEmpty()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return null;
+		}
 		Product product = new Product();
 		product.setName(name);
 		product.setIntroduction(introduction);
 		product.setPrice(price);
 		product.setImageUrl(file.getOriginalFilename());
 		service.create(product);
-		if (file.isEmpty()) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			return null;
-		}
 		// アップロードされたファイルを保存。
 		try {
 			BufferedInputStream in =
-					new BufferedInputStream(file.getInputStream());
+			new BufferedInputStream(file.getInputStream());
 			BufferedOutputStream out =
-					new BufferedOutputStream(new FileOutputStream("src/main/resources/static/image/" + file.getOriginalFilename()));
+			new BufferedOutputStream(new FileOutputStream("src/main/resources/static/image/" + file.getOriginalFilename()));
 			FileCopyUtils.copy(in, out);
 		} catch (IOException e) {
 			throw new RuntimeException("Error uploading file.", e);
